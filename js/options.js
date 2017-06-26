145494744626ff39aa79235a49a767efd27092af
Malte Kiefer <malte.kiefer@mailgermania.de>
2017-06-26 06:05:08 +0200

    start with new layout

--- js/options.js
+++ js/options.js
@@ -118,7 +118,6 @@ $(document).ready(function() {
 			var createOptions = {
 				numBits: 4096,
 				userIds: [{name:user}],
-				keyExpirationTime: oneYear,
 				passphrase: createdString
 			};
 			
